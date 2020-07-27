b770fd49924d67cdc3e4f418de98ebb1543f74e7
nirjanchhetri1 <nirajanchhetri6@gmail.com>
2020-07-28 00:14:24 +0545

    first commit

---
+++ clock.js
@@ -0,0 +1,15 @@
+setInterval(() => {
+    let date = new Date()
+let hour = date.getHours()
+let min =date.getMinutes()
+let sec =date.getSeconds()
+if(hour>12){ 
+
+    hour=hour%12
+}
+document.querySelector(".hour p").innerHTML=hour
+document.querySelector(".min p").innerHTML=min
+document.querySelector(".sec p").innerHTML=sec
+
+
+})
