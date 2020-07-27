48e9f1a321661f70971906fc7422efd028659095
nirjanchhetri1 <nirajanchhetri6@gmail.com>
2020-07-28 00:23:12 +0545

    12 edit

--- clock.js
+++ clock.js
@@ -1,15 +1,18 @@
 setInterval(() => {
     let date = new Date()
-let hour = date.getHours()
-let min =date.getMinutes()
-let sec =date.getSeconds()
-if(hour>12){ 
+    let hour = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+    if (hour > 12) {
 
-    hour=hour%12
-}
-document.querySelector(".hour p").innerHTML=hour
-document.querySelector(".min p").innerHTML=min
-document.querySelector(".sec p").innerHTML=sec
+        hour = hour % 12
+    }
+    if(hour == 0) {
+        hour = 12
+    }
+    document.querySelector(".hour p").innerHTML = hour
+    document.querySelector(".min p").innerHTML = min
+    document.querySelector(".sec p").innerHTML = sec
 
 
 })
